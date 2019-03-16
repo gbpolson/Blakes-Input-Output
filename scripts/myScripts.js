b3de22ef72c3b451ef1ea8a34dd5c3ab33304c1e
gbpolson <47468010+gbpolson@users.noreply.github.com>
2019-03-15 22:20:25 -0500

    Add files via upload

---
+++ scripts/myScripts.js
@@ -0,0 +1,9 @@
+/**
+ * Blake's Java In and Out Website Test Page
+ * index.scss
+ * - Add any styles you want here!
+ */
+
+var name = prompt("Please enter your name:");
+alert("Welcome " + name + "! We are glad to have you.");
+console.log("You entered " + name + ".");
